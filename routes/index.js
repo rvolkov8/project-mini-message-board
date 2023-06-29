2e3517838805399e803c5b071ea1c648ccab2a15
Rostyslav <volkov.rostyslav.8@gmail.com>
2023-06-29 20:15:26 +0300

    Implement simple functionality
    Users can see all the messages on the homepage as well as add their own messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message App', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Mini Message App' });
+});
+
+router.post('/new', (req, res) => {
+  const reqBody = req.body;
+  messages.push({
+    text: reqBody.userMessage,
+    user: reqBody.userName,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
